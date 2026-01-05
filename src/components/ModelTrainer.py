f633e6dbca70a6290b3009b8cbec5fdb422e9d85
BrainOnSteroids <63648245+khal33d-hub@users.noreply.github.com>
2026-01-05 23:40:34 +0000

    hyperparameter and logging

--- src/components/ModelTrainer.py
+++ src/components/ModelTrainer.py
@@ -37,8 +37,26 @@ class ModelTrainer:
                 "Lasso Regression": Lasso(),
             }
 
+            params = {
+                "Linear Regression": {
+                # usually nothing to tune here for basic LinearRegression
+                # optionally: "fit_intercept": [True, False]
+                },
+                "Ridge Regression": {
+                "alpha": [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0],
+                "fit_intercept": [True, False],
+                "solver": ["auto", "svd", "cholesky", "lsqr", "sag", "saga", "lbfgs"],
+                "max_iter": [1000, 5000, 10000],
+                },
+                "Lasso Regression": {
+                "alpha": [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0],
+                "fit_intercept": [True, False],
+                "max_iter": [1000, 5000, 10000],
+                "selection": ["cyclic", "random"],
+                }
+}
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models, param=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -53,6 +71,8 @@ class ModelTrainer:
             if best_model_score>5:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model is {best_model_name} with r2 score: {best_model_score}")
+            logging.info(f"best model parameters: {best_model.get_params()}")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
